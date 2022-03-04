687f7303dd3c307b1cd0fea223e3c360db0addee
pustoFrankYang <yang_chenxi@outlook.com>
2022-03-04 13:19:45 +0000

    Time: 1444 ms (37.51%), Space: 27.9 MB (59.67%) - LeetHub

---
+++ 565-array-nesting/565-array-nesting.py
@@ -0,0 +1,15 @@
+class Solution:
+    # it will form a loop! 
+    # and no outer element can enter the loop, wegens permutation!
+    def arrayNesting(self, nums: List[int]) -> int:
+        visited = [0] * len(nums)
+        maxlen = currlen = 0
+        for i in range(len(nums)):
+            j = i
+            while not visited[j]:
+                currlen += 1
+                visited[j] = 1
+                j = nums[j]
+            maxlen = max(maxlen, currlen)
+            currlen = 0
+        return maxlen
