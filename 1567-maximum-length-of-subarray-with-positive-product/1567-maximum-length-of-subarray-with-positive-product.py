4e13f798c194e4aa8e6bb4ebc45b69ab464d53c8
pustoFrankYang <yang_chenxi@outlook.com>
2022-03-04 02:27:39 +0000

    Time: 664 ms (83.43%), Space: 27.6 MB (94.45%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -1,21 +1,28 @@
 # WA [5,-20,-20,-39,-5,0,0,0,36,-32,0,-7,-10,-7,21,20,-12,-34,26,2]
+# WA [1000000000]
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
         n = len(nums)
         # dp positiv or nega, for subarr ending in i
-        dpp = [0] * n
-        dpn = [0] * n
+        # now change dp into two var
+        dpp = dpn = 0
         if nums[0] > 0:
-            dpp[0] = 1
+            dpp = 1
         elif nums[0] < 0:
-            dpn[0] = 1
+            dpn = 1
+        maxdpp = dpp  #### WA [1000000000]
         for i in range(1, n):
             if nums[i] > 0:
-                dpp[i] = dpp[i - 1] + 1
-                dpn[i] = dpn[i - 1] + 1 if dpn[i - 1] > 0 else 0
+                if dpn > 0:
+                    dpp, dpn = dpp + 1, dpn + 1
+                else:
+                     dpp, dpn = dpp + 1, 0
             elif nums[i] < 0:
-                dpp[i] = dpn[i - 1] + 1 if dpn[i - 1] > 0 else 0  #### forgot
-                dpn[i] = dpp[i - 1] + 1
+                if dpn > 0:
+                    dpp, dpn = dpn + 1, dpp + 1
+                else:
+                    dpp, dpn = 0, dpp + 1
             else:
-                dpp[i] = dpn[i] = 0
-        return max(dpp)
+                dpp = dpn = 0
+            maxdpp = max(maxdpp, dpp)
+        return maxdpp
