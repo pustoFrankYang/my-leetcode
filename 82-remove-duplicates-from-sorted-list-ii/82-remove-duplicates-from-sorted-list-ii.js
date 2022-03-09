d03f46dd988b234c0b99fdd0456b5168b7a95df2
pustoFrankYang <yang_chenxi@outlook.com>
2022-03-09 15:57:40 +0000

    Time: 114 ms (36.71%), Space: 43.4 MB (63.95%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
@@ -13,7 +13,10 @@ var deleteDuplicates = function(head) {
     let lead = new ListNode(0, head);
     let p = lead; 
     let q = lead.next;
-    while ( q && q.next) {
+    while (q && q.next) {
+        // when it reaches [p:a,q:b,c], q goes to c, no q.next, so break;
+        // when it reaches [p:a,q:b,b], q go until no q.next, 
+        //                            and p.next is set to null, break;
         if (p.next.val != q.next.val)
             p = p.next, q = p.next;
         else {
