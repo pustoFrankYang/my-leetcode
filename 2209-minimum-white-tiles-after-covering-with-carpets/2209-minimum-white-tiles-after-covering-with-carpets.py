e02188e419d2ad8da7af7573b7c324315a9f4935
pustoFrankYang <yang_chenxi@outlook.com>
2022-03-19 17:02:31 +0000

    Time: 8554 ms (11.11%), Space: 22.3 MB (100.00%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
@@ -15,13 +15,9 @@ class Solution:
                 #   add current tile, i.e. +1 when curr tile is white
                 jump = dp[i - 1][j] + 1 if floor[i - 1] == '1' else dp[i - 1][j]
                 
-                if j == 0:
-                    dp[i][j] = jump
-                else:
-                    # option 1: use new carpet
-                    #   put in front of curr tile
-                    #   when `i` is at beginning tiles, put carpet at 0;
-                    cover = dp[max(i - carpetLen, 0)][j - 1]
-                    
-                    dp[i][j] = min(cover, jump)
+                # option 1: use new carpet
+                #   put in front of curr tile
+                #   when `i` is at beginning tiles, put carpet at 0;
+                cover = dp[max(i - carpetLen, 0)][j - 1] if j != 0 else 1005
+                dp[i][j] = min(cover, jump)
         return dp[-1][-1]
