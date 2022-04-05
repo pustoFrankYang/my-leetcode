79cb400aeee4952dcdaa1dea9b108e457c2ff7df
pustoFrankYang <yang_chenxi@outlook.com>
2022-04-05 22:23:58 +0100

    Time: 137 ms (17.58%), Space: 50.2 MB (7.87%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -3,36 +3,29 @@
  * @return {number}
  */
 /*
-WA:
-[1,2,1]
+WA
+[2,3,10,5,7,8,9]
 */
-// ¡¡¡¡gesah,
-// cuz I-got-it-ve a fickende false one
-var maxArea = function(height) {
-    let L = 0, R = height.length - 1;
-    let tl = L, tr = R;
-    let ans = (R - L) * Math.min(height[L], height[R]);
-    while (true) {
-        // goto next state
-        // first, it is obvious that we only want to find a line that is higher than now.
-        // but which one should we move, L or R?
-        //   say, the lower is L, and higher is R
-        //   and there are L, newL, newR, R,
-        //   we are not interested in the pair (L, newR), 
-        //      because it can only be shorter than (L, R) but with same height
-        //   but we don't know pairs (newL, R) and (newL, newR), so we must calc in following steps
-        //   that's why it is only reasonable to move L to newL, say, L is lower
-        if (height[L] < height[R]) {
-            while (tl < tr && height[tl] <= height[L])
-                tl ++;
+var maxArea = function(h) {
+    let L = 0, R = h.length - 1;
+    let ans = (R - L) * Math.min(h[L], h[R]);
+    while (L < R) {
+        let nl = L, nr = R;
+        if (h[L] < h[R]) {
+            while (nl < R && h[nl] <= h[L])
+            nl ++;
+        L = nl;
+        ans = Math.max(ans, (R - L) * Math.min(h[L], h[R]));
         } else {
-            while (tl < tr && height[tr] <= height[R])
-                tr --;
+            while (nr > L && h[nr] <= h[R])
+            nr --;
+        R = nr;
+        ans = Math.max(ans, (R - L) * Math.min(h[L], h[R]));
         }
-        if (tl < tr) {
-            L = tl, R = tr;
-            ans = Math.max(ans, (R - L) * Math.min(height[L], height[R]));
-        } else break;
+        
+//         console.log(L, R)
+        
+//         // console.log(L, R)
     }
     return ans;
 };
