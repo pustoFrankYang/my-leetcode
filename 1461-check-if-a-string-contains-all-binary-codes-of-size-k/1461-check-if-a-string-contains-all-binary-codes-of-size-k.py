8b075ff231a8fa85a90621dff70d5faf0ae06245
EthanYangCX <yang_chenxi@outlook.com>
2022-05-31 19:57:46 +0100

    Time: 407 ms (73.89%), Space: 22.3 MB (92.92%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,8 +1,19 @@
+# class Solution:
+#     def hasAllCodes(self, s: str, k: int) -> bool:
+#         checker = 0
+#         for i in range(len(s) - k + 1):
+#             checker |= 1 << int(s[i:i + k], 2)
+#         return checker == 2 ** (2 ** k) - 1
+    
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        checker = 0
+        need = 2 ** k
+        got = [False] * need
         for i in range(len(s) - k + 1):
-            # print(s[i:i + k])
-            checker |= 1 << int(s[i:i + k], 2)
-        # print(bin(checker))
-        return checker == 2 ** (2 ** k) - 1
+            val = int(s[i:i + k], 2)
+            if not got[val]:
+                need -= 1
+                got[val] = True
+                if need == 0:
+                    return True
+        return False
