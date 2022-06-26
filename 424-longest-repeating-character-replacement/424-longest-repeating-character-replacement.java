9c7a7ac94f93f4dbe1774604e496a830f73883c7
Ethan Yang <yang_chenxi@outlook.com>
2022-06-26 23:12:33 +0100

    Time: 77 ms (16.85%), Space: 42.8 MB (72.79%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        List<Integer> cnt = new ArrayList<>();
+        for (int i = 0; i < 26; i ++)
+            cnt.add(0);
+        for (int i = 0; i < k; i ++) 
+            cnt.set(s.charAt(i) - 'A', cnt.get(s.charAt(i) - 'A') + 1);
+        int l = 0, r = k;
+        int currmax = r - l;
+        while (r < s.length()) {
+            // System.out.println(l + " " + r);
+            cnt.set(s.charAt(r) - 'A', cnt.get(s.charAt(r) - 'A') + 1);
+            r ++;
+            
+            while (l < r && Collections.max(cnt) + k < r - l) {
+                cnt.set(s.charAt(l) - 'A', cnt.get(s.charAt(l) - 'A') - 1);
+                l ++;
+            }
+            currmax = Math.max(currmax, r - l);
+        }
+        return currmax;
+    }
+}
