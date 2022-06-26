1491a3fee0211500113be7113dab5cf3bc81b37e
Ethan Yang <yang_chenxi@outlook.com>
2022-06-26 17:22:15 +0100

    Time: 19 ms (91.08%), Space: 54.1 MB (84.39%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -1,20 +1,12 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // sliding window of size k with hash map recording
-        Map<Integer, Integer> m = new HashMap<>();
-        int l = 0, r = 0;
-        for (; r < nums.length && r < k + 1; r ++) {  // !!! k + 1
-            if (m.getOrDefault(nums[r], 0) > 0)
-                return true;
-            m.put(nums[r], 1);
-        }
-        while (r < nums.length) {
-            m.put(nums[l], 0);
-            l ++;
-            if (m.getOrDefault(nums[r], 0) > 0)
-                return true;
-            m.put(nums[r], 1);
-            r ++;
+        // sliding window of size k with hash set recording
+        Set<Integer> s = new HashSet<>();
+        for (int i = 0; i < nums.length; i ++) {
+            if (s.contains(nums[i])) return true;
+            s.add(nums[i]);
+            if (s.size() > k)
+                s.remove(nums[i - k]);  // cuz fixed len
         }
         return false;
     }
