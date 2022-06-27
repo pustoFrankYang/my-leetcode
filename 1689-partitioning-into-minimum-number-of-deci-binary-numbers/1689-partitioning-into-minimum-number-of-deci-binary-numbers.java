743c7785ea2ef64c66b0dc456ec76e22186034ce
Ethan Yang <yang_chenxi@outlook.com>
2022-06-27 01:43:31 +0100

    Time: 9 ms (63.10%), Space: 53.7 MB (78.32%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minPartitions(String n) {
+        int ans = 0;
+        for (int i = 0; i < n.length(); i ++)
+            ans = Math.max(ans, n.charAt(i) - '0');
+        return ans;
+    }
+}
