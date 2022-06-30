004b754294daa67b79930c4d9a64a9d604514f03
Ethan Yang <yang_chenxi@outlook.com>
2022-06-30 18:33:24 +0100

    Time: 71 ms (67.77%), Space: 14.2 MB (20.83%) - LeetHub

---
+++ 457-circular-array-loop/457-circular-array-loop.py
@@ -0,0 +1,43 @@
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n = len(nums)
+        visited = [0] * n
+        for i in range(n):
+            if visited[i]:
+                continue
+            # no visited, let's check cycle
+            f = s = i
+            while nums[(f + nums[f]) % n] * nums[f] > 0:
+                print(f, s)
+                f = (f + nums[f]) % n
+                visited[f] = 1
+                if nums[(f + nums[f]) % n] * nums[f] < 0:
+                    break
+                elif f == s:
+                    # length
+                    if (f + nums[f]) % n != f:
+                        return True
+                    else:
+                        break
+                        
+                f = (f + nums[f]) % n
+                visited[f] = 1
+                if nums[(f + nums[f]) % n] * nums[f] < 0:
+                    break
+                elif f == s:
+                    # length
+                    if (f + nums[f]) % n != f:
+                        return True
+                    else:
+                        break
+                    
+                s = (s + nums[s]) % n
+        return False
+        
+'''
+WA:
+[-1,-2,-3,-4,-5]
+
+# loop end indent wrong
+[3,1,2]
+'''
