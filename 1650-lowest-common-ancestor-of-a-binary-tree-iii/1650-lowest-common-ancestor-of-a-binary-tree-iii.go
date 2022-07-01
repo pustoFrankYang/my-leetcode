e6c5a76bcb729b434695faa4f893ee2f4ffb4e01
Ethan Yang <yang_chenxi@outlook.com>
2022-07-01 18:28:30 +0100

    Time: 2 ms (86.73%), Space: 6.9 MB (22.45%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.go
@@ -0,0 +1,36 @@
+/**
+ * Definition for Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Parent *Node
+ * }
+ */
+
+func lowestCommonAncestor(p *Node, q *Node) *Node {
+    // cuz val unique
+    got := make(map[int]bool)
+    got[p.Val] = true
+    got[q.Val] = true
+    
+    for p.Parent != nil || q.Parent != nil {
+        if p.Parent != nil {
+            p = p.Parent
+            if got[p.Val] {
+                return p
+            } else {
+                got[p.Val] = true
+            }
+        }
+        if q.Parent != nil {
+            q = q.Parent
+            if got[q.Val] {
+                return q
+            } else {
+                got[q.Val] = true
+            }
+        }
+    }
+    return nil
+}
