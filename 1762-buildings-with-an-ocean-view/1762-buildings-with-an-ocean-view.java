a1b652a834c1b0b886d7004ae33fefa0d74c3892
Ethan Yang <yang_chenxi@outlook.com>
2022-07-02 14:43:40 +0100

    Time: 19 ms (39.25%), Space: 83.4 MB (70.70%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -1,17 +1,15 @@
+// Q1762m, runtime 99.90%
 class Solution {
     public int[] findBuildings(int[] heights) {
-        int currmax = 0, nextind = 0;
-        int[] res = new int[heights.length];
+        int currmax = 0;
+        List<Integer> res = new ArrayList<>();
         for (int i = heights.length - 1; i >= 0; i --) {
             if (heights[i] > currmax) {
-                res[nextind] = i;
-                nextind ++;
+                res.add(i);
             }
             currmax = currmax >= heights[i] ? currmax : heights[i];
         }
-        int[] ans = new int[nextind];
-        for (int i = nextind - 1; i >= 0; i --)
-            ans[nextind - i - 1] = res[i];
-        return ans;
+        Collections.reverse(res);
+        return res.stream().mapToInt(i -> i).toArray();
     }
 }
