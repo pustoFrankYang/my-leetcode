b6030f832e0e002e56bd2b00712ab4c3742f572b
Ethan Yang <yang_chenxi@outlook.com>
2022-07-03 21:51:29 +0100

    Time: 2743 ms (23.20%), Space: 45.3 MB (43.02%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        p, q = -1, -1
+        cur = 0
+        ans = [0] * len(nums)
+        while cur < len(nums):
+            p += 1
+            while nums[p] < 0:
+                p += 1
+            ans[cur] = nums[p]
+            cur += 1
+            
+            q += 1
+            while nums[q] > 0:
+                q += 1
+            ans[cur] = nums[q]
+            cur += 1
+        return ans
