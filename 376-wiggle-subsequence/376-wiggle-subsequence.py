d87227b038d1f661b11eafde1f929f7afb2cabe5
Ethan Yang <yang_chenxi@outlook.com>
2022-07-03 21:34:04 +0100

    Time: 46 ms (70.30%), Space: 14 MB (10.83%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    # just start from 1st element and change direc whenever it can change
+    # say A, B, C
+    # if A == B, then ans(A,B,C) == ans(B,C)
+    # if A > B and B > C, then ans = 1
+    # if A > B and B < C, then ans = 2
+    # so always start with A for whatever sequence
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+        prevdiff = nums[1] - nums[0]
+        cnt = 1 if prevdiff == 0 else 2
+        for i in range(2, len(nums)):
+            diff = nums[i] - nums[i - 1]
+            if (diff > 0 and prevdiff <= 0) or (diff < 0 and prevdiff >= 0):
+                cnt += 1
+                prevdiff = diff
+        return cnt
