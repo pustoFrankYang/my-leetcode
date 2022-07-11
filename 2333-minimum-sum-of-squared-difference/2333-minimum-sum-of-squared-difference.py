16b2922d1f813471fd9bc002ffdfceb2fbfba350
Ethan Yang <yang_chenxi@outlook.com>
2022-07-12 00:44:27 +0100

    Time: 5228 ms (9.18%), Space: 31.7 MB (99.16%) - LeetHub

---
+++ 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.py
@@ -0,0 +1,17 @@
+# ¡¡¡gesah
+class Solution:
+    def minSumSquareDiff(self, nums1: List[int], nums2: List[int], k1: int, k2: int) -> int:
+        chance = k1 + k2
+        h = [-abs(nums1[i] - nums2[i]) for i in range(len(nums1))]
+        if -sum(h) < chance:
+            return 0
+        heapq.heapify(h)
+        n = len(nums1)
+        while chance > 0:
+            curr = -heapq.heappop(h)
+            reducer = max(chance // n, 1)
+            curr -= reducer
+            chance -= reducer
+            heapq.heappush(h, -curr)
+        return sum(x**2 for x in h)
+                
