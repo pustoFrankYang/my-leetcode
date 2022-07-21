6dd8e56e30363e86e306a6314541e837ebce638e
Ethan Yang <yang_chenxi@outlook.com>
2022-07-22 00:07:41 +0100

    Time: 59 ms (32.59%), Space: 13.9 MB (80.43%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.py
@@ -0,0 +1,12 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        ans = []
+        for part in path.split('/'):
+            if part == '..':
+                if ans:
+                    ans.pop()
+            elif part == '':
+                continue
+            elif part != '.':
+                ans.append(part)
+        return '/' + '/'.join(ans)
