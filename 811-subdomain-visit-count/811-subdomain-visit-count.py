5d23c16ff24c645a466d3e4f1f91bed33a44b045
Ethan Yang <yang_chenxi@outlook.com>
2022-07-21 01:11:03 +0100

    Time: 74 ms (63.80%), Space: 14 MB (7.05%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        m = {}
+        for s in cpdomains:
+            cnt, do = s.split(' ')
+            cnt = int(cnt)
+            dos = do.split('.')
+            m[dos[-1]] = m.get(dos[-1], 0) + cnt
+            m[dos[-2] + '.' + dos[-1]] = m.get(dos[-2] + '.' + dos[-1], 0) + cnt
+            if len(dos) == 3:
+                m[dos[-3] + '.' + dos[-2] + '.' + dos[-1]] = m.get(dos[-3] + '.' + dos[-2] + '.' + dos[-1], 0) + cnt
+        ans = []
+        for k, v in m.items():
+            ans.append(str(v) + ' ' + k)
+        return ans
