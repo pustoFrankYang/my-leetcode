b010441c0ed1f04fc18d2ad3bc3f9be8e485373f
Ethan Yang <yang_chenxi@outlook.com>
2022-07-22 00:08:51 +0100

    Time: 44 ms (70.49%), Space: 14 MB (9.33%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,10 +1,10 @@
-# Q71m, 32.59%
+# Q71m, 91.13%
 class Solution:
     def simplifyPath(self, path: str) -> str:
         ans = []
         for part in path.split('/'):
             if part == '..':
-                if ans:
+                if ans:  # cuz [] can't pop()
                     ans.pop()
             elif part == '':
                 continue
