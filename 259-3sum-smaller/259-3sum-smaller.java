3f571c38008c25d360113f2183931cdb7d7d3353
Ethan Yang <yang_chenxi@outlook.com>
2022-07-22 21:27:02 +0100

    Time: 153 ms (8.01%), Space: 43.2 MB (47.33%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.java
+++ 259-3sum-smaller/259-3sum-smaller.java
@@ -6,11 +6,10 @@ class Solution {
         for (int i = 0; i < n - 2; i ++) {
             for (int j = i + 1; j < n - 1; j ++) {
                 int complement = target - nums[i] - nums[j];
+                // binsear of java is first posi greater than findee or any equal
                 int idx = Arrays.binarySearch(nums, j + 1, n - 1, complement);
-                // System.out.println(idx);
                 int hi = idx >= 0 ? idx : -idx;
                 int lo = hi - 1;
-                // System.out.println("" + i + j + lo);
                 while (lo > j && nums[lo] >= complement) lo --;
                 if (lo > j && nums[lo] < complement)
                     ans += lo - j;
