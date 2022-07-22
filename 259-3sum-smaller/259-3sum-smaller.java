bdbcfbebbcb977f1bd8d939b820c656f2ce3f65d
Ethan Yang <yang_chenxi@outlook.com>
2022-07-22 21:33:56 +0100

    Time: 9 ms (96.66%), Space: 42.2 MB (84.05%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.java
+++ 259-3sum-smaller/259-3sum-smaller.java
@@ -1,19 +1,19 @@
-// Q259m, runtime 8.01%
+// Q259m, runtime 8.14%
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         int ans = 0;
         int n = nums.length;
         Arrays.sort(nums);
         for (int i = 0; i < n - 2; i ++) {
-            for (int j = i + 1; j < n - 1; j ++) {
-                int complement = target - nums[i] - nums[j];
-                // binsear of java is first posi greater than findee or any equal
-                int idx = Arrays.binarySearch(nums, j + 1, n - 1, complement);
-                int hi = idx >= 0 ? idx : -idx;
-                int lo = hi - 1;
-                while (lo > j && nums[lo] >= complement) lo --;
-                if (lo > j && nums[lo] < complement)
-                    ans += lo - j;
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum >= target - nums[i]) {
+                    r --;
+                } else {
+                    ans += r - l;
+                    l ++;
+                }
             }
         }
         return ans;
