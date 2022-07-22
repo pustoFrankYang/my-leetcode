69f88e99f424bbc1c87fda4f7ea19e65bd5487f4
Ethan Yang <yang_chenxi@outlook.com>
2022-07-22 20:50:06 +0100

    Time: 421 ms (5.02%), Space: 42.7 MB (80.78%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -2,20 +2,21 @@
 // Q16m, runtime 5.22%
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int diff = Integer.MAX_VALUE;
-        int sz = nums.length;
+        int ans = nums[0] + nums[1] + nums[2];
+        int n = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < sz && diff != 0; ++i) {
-            for (int j = i + 1; j < sz - 1; ++j) {
+        for (int i = 0; i < n - 2 && ans != target; i ++) {
+            for (int j = i + 1; j < n - 1; j ++) {
                 int complement = target - nums[i] - nums[j];
-                var idx = Arrays.binarySearch(nums, j + 1, sz - 1, complement);
-                int hi = idx >= 0 ? idx : ~idx, lo = hi - 1;
-                if (hi < sz && Math.abs(complement - nums[hi]) < Math.abs(diff))
-                    diff = complement - nums[hi];
-                if (lo > j && Math.abs(complement - nums[lo]) < Math.abs(diff))
-                    diff = complement - nums[lo];
+                int idx = Arrays.binarySearch(nums, j + 1, n - 1, complement);
+                int hi = idx >= 0 ? idx : -idx;
+                int lo = hi - 1;
+                if (hi < n && Math.abs(complement - nums[hi]) < Math.abs(target - ans))
+                    ans = nums[i] + nums[j] + nums[hi];
+                if (lo < n && Math.abs(complement - nums[lo]) < Math.abs(target - ans))
+                    ans = nums[i] + nums[j] + nums[lo];
             }
         }
-        return target - diff;
+        return ans;
     }
 }
