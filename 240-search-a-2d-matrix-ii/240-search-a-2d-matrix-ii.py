b8ab45a87add6c772345075b3a04ee85199f3866
Ethan Yang <yang_chenxi@outlook.com>
2022-07-24 16:46:51 +0100

    Time: 313 ms (26.45%), Space: 20.5 MB (40.22%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        # caution how to extract a column
+        maxrow = bisect.bisect([r[0] for r in matrix], target)
+        minrow = bisect.bisect_left([r[-1] for r in matrix], target) - 1
+        if minrow < 0: minrow = 0
+        if maxrow >= m: maxrow = m - 1
+        
+        for r in range(minrow, maxrow + 1):
+            ind = bisect.bisect_left(matrix[r], target)
+            if ind < n and matrix[r][ind] == target:
+                return True
+        return False
+    
+'''
+[[1,3,5]]
+5
+'''
