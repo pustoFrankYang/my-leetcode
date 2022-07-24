46f3f592dad630646c112a717f75cc032ae46494
Ethan Yang <yang_chenxi@outlook.com>
2022-07-24 04:14:47 +0100

    Time: 50 ms (33.33%), Space: 13.9 MB (33.33%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.py
@@ -0,0 +1,10 @@
+class Solution:
+    def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+        ranks.sort()
+        if all(suits[0] == suit for suit in suits):
+            return 'Flush'
+        if ranks[0] == ranks[1] == ranks[2] or ranks[1] == ranks[2] == ranks[3] or ranks[2] == ranks[3] == ranks[4]:
+            return 'Three of a Kind'
+        if ranks[0] == ranks[1] or ranks[1] == ranks[2] or ranks[2] == ranks[3] or ranks[3] == ranks[4]:
+            return 'Pair'
+        return 'High Card'
