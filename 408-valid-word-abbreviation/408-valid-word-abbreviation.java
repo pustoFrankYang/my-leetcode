6462b9371c0b1d192394317edb27961ddd83d351
Ethan Yang <yang_chenxi@outlook.com>
2022-07-27 17:57:10 +0100

    Time: 14 ms (5.67%), Space: 42.8 MB (13.37%) - LeetHub

--- 408-valid-word-abbreviation/408-valid-word-abbreviation.java
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.java
@@ -2,7 +2,8 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
         int p = 0, q = 0;
-        while (p < word.length() && q < abbr.length()) {
+        int wordLen = word.length(), abbrLen = abbr.length();
+        while (p < wordLen && q < abbrLen) {
             if (abbr.charAt(q) > '9') {
                 if (word.charAt(p) != abbr.charAt(q))
                     return false;
@@ -10,16 +11,16 @@ class Solution {
                 q ++;
             } else {
                 String num = "";
-                while (q < abbr.length() && abbr.charAt(q) <= '9') {
+                while (q < abbrLen && abbr.charAt(q) <= '9') {
                     num += abbr.charAt(q);
                     q ++;
                 }
                 if (num.charAt(0) == '0') return false;
                 p += Integer.parseInt(num);
-                if (p > word.length()) return false;
+                if (p > wordLen) return false;
             }
         }
-        return p >= word.length() && q >= abbr.length();
+        return p >= wordLen && q >= abbrLen;
     }
 }
 /* 
