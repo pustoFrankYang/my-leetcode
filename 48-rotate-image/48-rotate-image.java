8d2f77731f40b37acd8c80e83cc8128d582ff1d1
Ethan Yang <yang_chenxi@outlook.com>
2022-08-30 19:32:48 +0100

    Time: 0 ms (100.00%), Space: 42.7 MB (57.35%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[][] m) {
+        int n = m.length;
+        int ilimit = (n + 1) / 2, jlimit = n / 2;
+        for (int i = 0; i < ilimit; i++) {
+            for (int j = 0; j < jlimit; j++) {
+                int t = m[i][j];
+                m[i][j] = m[n - 1 - j][i];
+                m[n - 1 - j][i] = m[n - 1 - i][n - 1 - j];
+                m[n - 1 - i][n - 1 - j] = m[j][n - 1 - i];
+                m[j][n - 1 - i] = t;
+            }
+        }
+    }
+}
