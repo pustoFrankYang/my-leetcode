7d31b79c9c06705a6fba12de4874d9bc9a2c0d00
Ethan Yang <yang_chenxi@outlook.com>
2022-08-31 22:09:55 +0100

    Time: 8 ms (65.70%), Space: 43.4 MB (94.31%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,8 +1,23 @@
 // 注意，不是直线流，是可以曲线，但不能 3m->1m->2m
+// Q417m, runtime 65.70%
 class Solution {
+    int m, n;
+    int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public void propagate(boolean[][] isOcn, Queue<int[]> ocnQ, int[][] heights) {
+        while (!ocnQ.isEmpty()) {
+            int[] curr = ocnQ.poll();
+            for (var dir : dirs) {
+                int nx = curr[0] + dir[0], ny = curr[1] + dir[1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                if (!isOcn[nx][ny] && heights[nx][ny] >= heights[curr[0]][curr[1]]) {
+                    isOcn[nx][ny] = true;
+                    ocnQ.offer(new int[]{nx, ny});
+                }
+            }
+        }
+    }
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int m = heights.length, n = heights[0].length;
+        m = heights.length; n = heights[0].length;
         boolean[][] isPac = new boolean[m][n], isAtl = new boolean[m][n];
         Queue<int[]> pacQ = new LinkedList<>(), atlQ = new LinkedList<>();
         for (int i = 0; i < m; i ++) {
@@ -17,28 +32,10 @@ class Solution {
             isAtl[m - 1][i] = true;
             atlQ.offer(new int[]{m - 1, i});
         }
-        while (!pacQ.isEmpty()) {
-            int[] curr = pacQ.poll();
-            for (var dir : dirs) {
-                int nx = curr[0] + dir[0], ny = curr[1] + dir[1];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-                if (!isPac[nx][ny] && heights[nx][ny] >= heights[curr[0]][curr[1]]) {
-                    isPac[nx][ny] = true;
-                    pacQ.offer(new int[]{nx, ny});
-                }
-            }
-        }
-        while (!atlQ.isEmpty()) {
-            int[] curr = atlQ.poll();
-            for (var dir : dirs) {
-                int nx = curr[0] + dir[0], ny = curr[1] + dir[1];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-                if (!isAtl[nx][ny] && heights[nx][ny] >= heights[curr[0]][curr[1]]) {
-                    isAtl[nx][ny] = true;
-                    atlQ.offer(new int[]{nx, ny});
-                }
-            }
-        }
+        
+        propagate(isPac, pacQ, heights);
+        propagate(isAtl, atlQ, heights);
+        
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < m; i ++)
             for (int j = 0; j < n; j ++)
