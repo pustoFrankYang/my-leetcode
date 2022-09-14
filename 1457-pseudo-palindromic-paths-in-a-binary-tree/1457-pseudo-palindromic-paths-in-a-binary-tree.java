e53343c4bf3a1b120a8306df390e6f62228997e4
Ethan Yang <yang_chenxi@outlook.com>
2022-09-15 00:17:42 +0100

    Time: 18 ms (78.79%), Space: 104.9 MB (78.51%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // palin means only 0 or 1 of the cnts are odd
+    private int[] cnts = new int[10];
+    private int ans = 0;
+    private void dfs(TreeNode node) {
+        if (node.left == null && node.right == null) {
+            int cntOdd = 0;
+            for (int i = 1; i <= 9; i ++) {
+                if (cnts[i] % 2 == 1) cntOdd ++;
+                if (cntOdd >= 2) return;
+            }
+            ans += 1;
+            return;
+        }
+        if (node.left != null) {
+            cnts[node.left.val] ++;
+            dfs(node.left);
+            cnts[node.left.val] --;
+        }
+        if (node.right != null) {
+            cnts[node.right.val] ++;
+            dfs(node.right);
+            cnts[node.right.val] --;
+        }
+    }
+    public int pseudoPalindromicPaths (TreeNode root) {
+        cnts[root.val] ++;
+        dfs(root);
+        return ans;
+    }
+}
