d9034e4984847d7f19bc303567f372a846aa52e5
Ethan Yang <yang_chenxi@outlook.com>
2022-09-16 20:48:03 +0100

    Time: 375 ms (81.25%), Space: 89.1 MB (40.63%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.ts
@@ -0,0 +1,17 @@
+// Q1770m, runtime, TS
+function maximumScore(nums: number[], multipliers: number[]): number {
+    var n = nums.length, m = multipliers.length;
+    var dp = [] as number[][];
+    for (let i = 0; i < m + 1; i ++) {
+        dp.push(new Array(m + 1).fill(0));
+    }
+    for (let op = m - 1; op > -1; op --)
+        for (let l = 0; l < op + 1; l ++) {
+            let r = n - 1 - op + l;
+            dp[l][op] = Math.max(
+                multipliers[op] * nums[l] + dp[l + 1][op + 1],
+                multipliers[op] * nums[r] + dp[l][op + 1]
+            )
+        }
+    return dp[0][0];
+};
