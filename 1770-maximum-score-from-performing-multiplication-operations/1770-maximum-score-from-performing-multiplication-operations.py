af7ec28ebd80ce9e0575a17eb9798099abc00e25
Ethan Yang <yang_chenxi@outlook.com>
2022-09-16 20:32:50 +0100

    Time: 9325 ms (13.96%), Space: 42.1 MB (16.14%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -0,0 +1,19 @@
+# Note: The python code of top-down DP may give Time Limit Exceeded/Memory Limit Exceeded since it has a larger constant factor.
+# Q1770m, TLE, py
+class Solution:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+        # l + (len (- 1) - r) = op; r = len - 1 - op + l
+        # so states are l and op
+        m, n = len(multipliers), len(nums)
+        # dp[the left choice][number of operations done]
+        dp = [[0 for _ in range(m + 1)] for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][m] = 0
+        for op in range(m - 1, -1, -1): # op reverse iter
+            for l in range(op + 1): # l any order but must within op
+                r = n - 1 - op + l
+                dp[l][op] = max(
+                    multipliers[op] * nums[l] + dp[l + 1][op + 1], 
+                    multipliers[op] * nums[r] + dp[l][op + 1]
+                               )
+        return dp[0][0]
