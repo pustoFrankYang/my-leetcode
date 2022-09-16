ea7e44335c563ae06f05cbcee3e1bdf23e302a97
Ethan Yang <yang_chenxi@outlook.com>
2022-09-16 02:52:23 +0100

    Time: 55 ms (13.44%), Space: 62.3 MB (17.68%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+        for (int i = 0; i < n - 1; i ++) {
+            s = say(s);
+        }
+        return s;
+    }
+    private String say(String s) {
+        String ans = "";
+        char curr = s.charAt(0);
+        int cnt = 1;
+        if (s.length() == 1) return "1" + s;
+        for (int i = 1; i < s.length(); i ++) {
+            if (s.charAt(i) == curr) {
+                cnt ++;
+            } else {
+                ans += Integer.toString(cnt) + curr;
+                curr = s.charAt(i);
+                cnt = 1;
+            }
+        }
+        ans += Integer.toString(cnt) + curr;
+        return ans;
+    }
+}
