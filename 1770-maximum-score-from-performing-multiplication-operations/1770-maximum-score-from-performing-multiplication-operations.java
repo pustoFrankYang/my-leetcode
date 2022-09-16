403bf8a9f44eef45221f262ba18aa326136a479b
Ethan Yang <yang_chenxi@outlook.com>
2022-09-16 20:55:01 +0100

    Time: 77 ms (71.73%), Space: 102.1 MB (74.98%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -0,0 +1,16 @@
+// Q1770m, runtime, java
+class Solution {
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int m = multipliers.length, n = nums.length;
+        int[][] dp = new int[m + 1][m + 1];
+        for (int op = m - 1; op > -1; op --)
+            for (int l = 0; l < op + 1; l ++) {
+                int r = n - 1 - op + l;
+                dp[l][op] = Math.max(
+                    multipliers[op] * nums[l] + dp[l + 1][op + 1],
+                    multipliers[op] * nums[r] + dp[l][op + 1]
+                );
+            }
+        return dp[0][0];
+    }
+}
