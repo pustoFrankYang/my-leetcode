de64d39087a15b338d0afcb69596360a573321c6
Ethan Yang <yang_chenxi@outlook.com>
2022-09-16 21:31:52 +0100

    Time: 7 ms (71.78%), Space: 42.5 MB (97.35%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -1,4 +1,4 @@
-// Q937m, runtime 12.08%, java
+// Q937m, runtime 32.65%, java
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
         List<String> letlogs = new ArrayList<>(), diglogs = new ArrayList<>();
