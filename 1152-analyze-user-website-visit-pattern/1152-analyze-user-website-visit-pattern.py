cc8fc56b3fc0af04ac97bb76780144e8fef36db8
Ethan Yang <yang_chenxi@outlook.com>
2022-09-16 22:04:54 +0100

    Time: 138 ms (7.09%), Space: 15.3 MB (12.32%) - LeetHub

---
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -0,0 +1,28 @@
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        # {pattern -> [usernames]}
+        p2u = defaultdict(set)
+        # user -> indexes
+        u2i = defaultdict(list)
+        for i in range(len(username)):
+            u2i[username[i]].append(i)
+        for u, inds in u2i.items():
+            inds = sorted(inds, key=lambda x : timestamp[x]) # !!! timestamp
+            webs = list(website[i] for i in inds)
+            n = len(webs)
+            if n < 3:
+                continue
+            for a in range(n - 2):
+                for b in range(a + 1, n - 1):
+                    for c in range(b + 1, n):
+                        p2u[str([webs[a], webs[b], webs[c]])].add(u)
+        maxs = 0
+        ps = []
+        for p, uns in p2u.items():
+            if len(uns) > maxs:
+                maxs = len(uns)
+                ps = [p]
+            elif len(uns) == maxs:
+                ps.append(p)
+        # change str repre of list into list
+        return sorted(ps)[0].replace("'", "").strip('[]').split(', ')
