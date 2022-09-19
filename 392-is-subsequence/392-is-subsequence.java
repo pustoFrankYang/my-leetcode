620f56f60973ea0f4e975d27bfe061aff415e100
Ethan Yang <yang_chenxi@outlook.com>
2022-09-19 02:28:10 +0100

    Time: 1 ms (91.85%), Space: 42.6 MB (13.60%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int p = 0, q = 0;
+        while (p < s.length() && q < t.length()) {
+            while (q < t.length() && s.charAt(p) != t.charAt(q))
+                q ++;
+            if (q < t.length() && s.charAt(p) == t.charAt(q)) {
+                p ++;
+                q ++;
+            }
+        }
+        if (p < s.length()) return false;
+        return true;
+    }
+}
