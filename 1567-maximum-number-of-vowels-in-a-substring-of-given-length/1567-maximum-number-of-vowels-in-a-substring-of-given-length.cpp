5584f69680c6821597350a576b77348f7e786cde
朵牛 Chauncey Young <i@yangchnx.com>
2023-05-05 15:12:47 +0100

    Time: 19 ms (81.96%), Space: 9.9 MB (68.38%) - LeetHub

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -0,0 +1,24 @@
+// C++复健, 试 LeetHub
+// Oh, LeetHub only work with OLD ver. LeetCode!!!
+// Can the new LeetHub V2 work with new UI ?
+class Solution {
+public:
+    bool isVowel(char c) {
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
+    int maxVowels(string s, int k) {
+        int p = 0, q = k - 1;
+        int curr_cnt = 0, max_cnt = 0;
+        for (int i = 0; i < k; i ++) {
+            if (isVowel(s[i]))
+                curr_cnt ++;
+        }
+        max_cnt = curr_cnt;
+        while (q < s.length() - 1) {
+            if (isVowel(s[p++])) curr_cnt --;
+            if (isVowel(s[++q])) curr_cnt ++;
+            if (max_cnt < curr_cnt) max_cnt = curr_cnt; 
+        }
+        return max_cnt;
+    }
+};
