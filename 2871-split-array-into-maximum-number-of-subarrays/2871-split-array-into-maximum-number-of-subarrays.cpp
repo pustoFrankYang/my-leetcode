2aa91d2eb795449bf67deeba83251c4f490ccd2a
朵牛 TwaqNgu <i@yangchnx.com>
2023-09-30 21:18:42 +0100

    Time: 88 ms (100.00%), Space: 105.1 MB (33.33%) - LeetHub

--- 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.cpp
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.cpp
@@ -9,12 +9,17 @@
 // 是故找零和者以为子序列
 
 // 不过，为什么从头遍历，遇0则止，即是最大分割数？
+// 我觉得可以假设，假如担心 ABCDE 不是最优分割，还能分成 XA BCD EY
+// 那么遍历时还是会先遇到这些分割，而不是先遇到 ABCDE
+// 因此边遍历边分割就是最优
+
 class Solution {
 public:
     int maxSubarrays(vector<int>& nums) {
         int res = 0, cur = INT_MAX;
         for (int n : nums) {
             cur &= n;
+            // 子序列终于至0，记录1，并复位
             if (cur == 0) {
                 ++res;
                 cur = INT_MAX;
