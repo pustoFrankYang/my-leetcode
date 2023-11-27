9b0e4f89daf2ebc2708ff1ac76425ee644b21fef
朵牛 TwaqNgu <i@yangchnx.com>
2023-11-27 10:10:21 +0000

    Time: 4232 ms (21.81%), Space: 16.5 MB (67.69%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        next = [[0] * 10 for _ in range(10)]
+        next[0][4] = next[0][6] = next[1][6] = next[1][8] = next[2][7] = next[2][9] = 1
+        next[3][4] = next[3][8] = next[4][0] = next[4][3] = next[4][9] = next[6][0] = 1
+        next[6][1] = next[6][7] = next[7][2] = next[7][6] = next[8][1] = next[8][3] = 1
+        next[9][2] = next[9][4] = 1
+        ans = 0
+        curr = [1] * 10
+        for _ in range(n - 1):
+            new = [0] * 10
+            for n in range(10):
+                new[n] = sum([curr[x] * next[n][x] for x in range(10)])
+            curr = new
+        return sum(curr) % (1000000007)
