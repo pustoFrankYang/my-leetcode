df811a0460cea61bf12203c641d5543eac12bd66
朵牛TwáŊu <i@yangchnx.com>
2024-02-06 10:03:44 +0000

    Time: 3 ms (43.24%), Space: 7.2 MB (21.34%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int currx = 0, curry = 0, currdir = 0;
+        // dirs arranged to clockwise
+        vector<pair<int, int>> dirs = {
+            {0, 1}, {1, 0}, {0, -1}, {-1, 0},
+        };
+        for (int i = 0; i < 4; ++i) {
+            for (const char& ch : instructions) {
+                switch (ch) {
+                    case 'G': 
+                        currx += dirs[currdir].first;
+                        curry += dirs[currdir].second;
+                        break;
+                    case 'L':
+                        currdir = (currdir - 1 + 4) % 4;
+                        break;
+                    case 'R':
+                        currdir = (currdir + 1) % 4;
+                        break;
+                }
+            }
+        }
+        return currx == 0 && curry == 0;
+    }
+};
