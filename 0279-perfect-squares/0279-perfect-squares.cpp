a98726d91ff04ea3adffb21f25e7fe377f9f5d64
朵牛TwáŊu <i@yangchnx.com>
2024-02-08 16:30:06 +0000

    Time: 111 ms (62.43%), Space: 10.3 MB (42.39%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -10,16 +10,16 @@ public:
         // but wherefrom the `i`?
         // check every squared numbers?
         // oh make sense cuz n <= 10000
-        vector<int> perfect_squares(101);
-        for (int i = 1; i <= 100; ++i) {
-            perfect_squares[i] = i * i;
-        }
+        // vector<int> perfect_squares(101);
+        // for (int i = 1; i <= 100; ++i) {
+        //     perfect_squares[i] = i * i;
+        // }
         
         // remember to init to int-max
         vector<int> dp(n + 1, INT_MAX);
         for (int i = 0; i <= n; ++i) {
-            for (int root = 100; root >= 1; --root) {
-                int sq = perfect_squares[root];
+            for (int j = 1; j <= sqrt(i); ++j) {
+                int sq = j * j;
                 if (sq == i) {
                     dp[i] = 1;
                 } else if (sq < i) {
