f5cb25291132daa69b183c0f294f7f99b3098860
朵牛TwáŊu <i@yangchnx.com>
2024-02-08 17:03:24 +0000

    Time: 0 ms (100.00%), Space: 8.1 MB (6.94%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,7 +3,6 @@ public:
     int lengthOfLastWord(string s) {
         stringstream ss(s);
         string word;
-        // 我日，把循环写成空的之后编译器就知道怎么优化了。。。？
         while (ss >> word) { };
         return word.size();
     }
