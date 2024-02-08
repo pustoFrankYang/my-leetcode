db5ed6d26c342bb709dca192326ada608379d72a
朵牛TwáŊu <i@yangchnx.com>
2024-02-08 16:22:00 +0000

    Time: 113 ms (62.12%), Space: 10.9 MB (28.71%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // O(100n) = O(n)
+    int numSquares(int n) {
+        // I guess we can use DP 
+        // cuz you can get the result smh from smaller number
+        // but seems not so straightforward...
+        // alreadies hold itself == itself: dp[1], dp[4], dp[9]...
+        // others can try dp[n] = dp[n - i] + dp[i]
+        // but wherefrom the `i`?
+        // check every squared numbers?
+        // oh make sense cuz n <= 10000
+        vector<int> perfect_squares(101);
+        for (int i = 1; i <= 100; ++i) {
+            perfect_squares[i] = i * i;
+        }
+        
+        vector<int> dp(n + 1);
+        for (int i = 0; i <= n; ++i) {
+            for (int root = 100; root >= 1; --root) {
+                int sq = perfect_squares[root];
+                if (sq == i) {
+                    dp[i] = 1;
+                } else if (sq < i) {
+                    dp[i] = dp[i] == 0? dp[sq] + dp[i - sq] : min(dp[i], dp[sq] + dp[i - sq]);
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
