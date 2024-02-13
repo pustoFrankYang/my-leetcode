eab7a868f37adace3d5a607b3cd3aebcbe30c45b
朵牛TwáŊu <i@yangchnx.com>
2024-02-13 01:10:38 +0000

    Time: 14 ms (64.45%), Space: 10.5 MB (45.10%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -3,19 +3,17 @@ public:
     // 灵机一动竟然还真对了，不错不错
     int minFlips(string target) {
         int len = target.size();
-        char prev = ' ';
         
         // a continuous sequence of the same digits can be flipped in one run
         // therefore we just count how many differernt such sequences there are
-        int cnt_continuous_digits = 0;
+        int cnt_continuous_digits = 1;
         // if first digit is 0, it means the first sequence don't need to be flipped
         // so there is 1 less run
         if (target[0] == '0') --cnt_continuous_digits;
         
-        for (int i = 0; i < len; ++i) {
-            if (target[i] != prev) {
+        for (int i = 1; i < len; ++i) {
+            if (target[i] != target[i - 1]) {
                 ++cnt_continuous_digits;
-                prev = target[i];
             }
         }
         return cnt_continuous_digits;
