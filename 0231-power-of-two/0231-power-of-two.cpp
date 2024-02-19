77ae49bac74fe96d70d8bc7c9283cc3e38641a0b
浦司图Pusto <i@yangchnx.com>
2024-02-19 08:33:40 +0000

    Time: 0 ms (100.00%), Space: 7.3 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -6,6 +6,7 @@ public:
         // 然后用题解里面学到的位运算的规律，与最后一个 1 的位有关的
         if (n <= 0) return false;
         long long x = n;
-        return (x & (x - 1)) == 0;
+        // return (x & (x - 1)) == 0;
+        return (x & (-x)) == x;
     }
 };
