637c74b690005c54a8080e8b2d2ca4971e2e4f49
浦司图Pusto <i@yangchnx.com>
2024-02-19 08:32:55 +0000

    Time: 0 ms (100.00%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 1) return true;
-        long long curr = 2;
-        for (int i = 0; i < 31; i++) {
-            if (n == curr) return true;
-            curr *= 2;
-        }
-        return false;
+        // 用位运算来解的话
+        // 我一开始想错了，想成它是 111111 了，其实是 1000000啊。。。
+        // 然后用题解里面学到的位运算的规律，与最后一个 1 的位有关的
+        if (n <= 0) return false;
+        long long x = n;
+        return (x & (x - 1)) == 0;
     }
 };
